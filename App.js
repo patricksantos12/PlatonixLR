2e8c70e67ff28c561cf11bcfb8abfc67925e2071
Patrick Santos <patricksantos2627@gmail.com>
2023-03-14 13:05:02 +0800

    second commit

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 // screens
 import Login from './screens/Login';
@@ -9,11 +7,3 @@ export default function App() {
   return <Login />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/Login.js
+++ screens/Login.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+
 
 import {
     StyledContainer,
     InnerContainer,
     PageLogo,
-    PageTitle
+    PageTitle,
+    SubTitle,
+    StyledFormArea
 } from './../components/styles';
 
 const Login = () => {
     return (
         <StyledContainer>
+            <StatusBar style="dark" />
             <InnerContainer>
                 <PageLogo resizeMode="cover" source={require('./../assets/img/Logo.png')}/>
                 <PageTitle>Platonix</PageTitle>
+                <SubTitle>Account Login</SubTitle>
             </InnerContainer>
         </StyledContainer>
     );
